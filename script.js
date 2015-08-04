fd6769c27e88d9dbc6093a1d408ff7bfad5cca54
duncangrubbs <duncan.grubbs@gmail.com>
2015-08-04 13:50:49 -0700

    Add admin form check and page.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function output() {
-	console.log("Function 'output' ran!");
-  alert("Thanks!");
-}
-
 function checkInput() {
+	var inputUsername = document.getElementById('inputUsername').value;
+	var inputPassword = document.getElementById('inputPassword').value;
 
+	if (inputUsername == "duncangrubbs" & inputPassword == "duncan11") {
+		console.log("test");
+		window.open("admin.html");
+	}
+	else {
+		alert("Please enter a correct username/password.");
+		return false;
+	}
+	
 }
