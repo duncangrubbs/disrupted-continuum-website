7960692520bea254c0ec919083dbb41fa3ec68d5
duncangrubbs <duncan.grubbs@gmail.com>
2015-08-05 17:45:01 -0700

    Add Small Changes.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//creating a function to check the admin login
 function checkInput() {
+	//setting user's input equal to variables
 	var inputUsername = document.getElementById('inputUsername').value;
 	var inputPassword = document.getElementById('inputPassword').value;
 
+	//checking if the username and password are correct
 	if (inputUsername == "duncangrubbs" & inputPassword == "duncan11") {
 		console.log("test");
 		window.open("admin.html");
