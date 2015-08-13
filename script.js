4135bd7d7570cd96a2cd8a0c0b49cb43ec826f4d
Duncan Grubbs <duncan.grubbs@gmail.com>
2015-08-13 00:23:21 -0700

    Remove Sebastian's code.

--- script.js
+++ script.js
@@ -10,18 +10,6 @@ function checkInput() {
 		console.log("test");
 		window.open("admin.html");
 
-	var inputUsername = document.getElementById('username').value;
-	var inputPassword = document.getElementById('password').value;
-
-	secret = "U2FsdGVkX1+KkU5/aGa4zooxG3anrWj0O91TtUvbz3gOTy+BKgkWq4JF1+7au5i9"
-	try {
-    document.getElementById('secret').innerHTML = GibberishAES.dec(secret, inputUsername + inputPassword);
-
 	}
-	catch(err) {
-			document.getElementById('secret').innerHTML = ""
-	    alert("Please enter a correct username/password.");
-	}
-
 
 }
