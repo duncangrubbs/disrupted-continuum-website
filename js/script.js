6916ad9539cf8c73f186692350543a1b3cc569f4
duncangrubbs <duncan.grubbs@gmail.com>
2015-10-24 21:42:43 -0700

    Clean everything up.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-//creating a function to check the admin login
-function checkInput() {
-
-	//setting user's input equal to variables
-	var inputUsername = document.getElementById('inputUsername').value;
-	var inputPassword = document.getElementById('inputPassword').value;
-
-	//checking if the username and password are correct
-	if (inputUsername == "duncangrubbs" & inputPassword == "duncan11") {
-		alert("test");
-		window.open("admin.html");
-
-	}
-
+function addToCart(){
+	document.write('Item has been added!');
 }
