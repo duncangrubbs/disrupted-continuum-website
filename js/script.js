5c97472bc81eacf6f7b8fc85d9b3b6f6c74d27a5
Duncan Grubbs <duncan.grubbs@gmail.com>
2015-10-30 12:06:42 -0700

    Add javascript comments.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+//Define function
 function addToCart(){
 	num = 0;
 
+	//Define Local storage items
 	localStorage.setItem("string", "Cart: 0");
 	localStorage.setItem("number", "");
 
+	//Attempt to convert a local string to an int
 	num = localStorage.getItem("number");
 	number = parseInt(num);
 	number++;
 
+	//Write final to screen
 	document.getElementById("cart-number").innerHTML = localStorage.getItem("string" + "number");
 
 	//localStorage.setItem = ("stringTwo", '<img id="product-4" src="images/sticker.png" class="img img-responsive">');
