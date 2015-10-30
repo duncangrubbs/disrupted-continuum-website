bb296ba761d990f4698c91821c60e0a98b249f08
Duncan Grubbs <duncan.grubbs@gmail.com>
2015-10-30 12:03:20 -0700

    Add basic cart function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 function addToCart(){
-	document.write('Item has been added!');
+	num = 0;
+
+	localStorage.setItem("string", "Cart: 0");
+	localStorage.setItem("number", "");
+
+	num = localStorage.getItem("number");
+	number = parseInt(num);
+	number++;
+
+	document.getElementById("cart-number").innerHTML = localStorage.getItem("string" + "number");
+
+	//localStorage.setItem = ("stringTwo", '<img id="product-4" src="images/sticker.png" class="img img-responsive">');
+	//document.getElementById("merch-1-cart").innerHTML = localStorage.getItem("stringTwo");
+
 }
