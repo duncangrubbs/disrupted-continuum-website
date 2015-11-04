a546b6d7782c70c7f8e0dac908c841733c648a50
Duncan Grubbs <duncan.grubbs@gmail.com>
2015-11-04 11:59:21 -0800

    Attempt to fix JS.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 //Define function
 function addToCart(){
-	num = 0;
-
 	//Define Local storage items
-	localStorage.setItem("string", "Cart: 0");
-	localStorage.setItem("number", "");
-
-	//Attempt to convert a local string to an int
-	num = localStorage.getItem("number");
-	number = parseInt(num);
-	number++;
+	localStorage.setItem("string", "Cart: ");
+	localStorage.setItem("number", "1");
+	var num = +localStorage.getItem('number');
+	num++;
 
 	//Write final to screen
-	document.getElementById("cart-number").innerHTML = localStorage.getItem("string" + "number");
+	document.getElementById("cart-number").innerHTML = localStorage.getItem("string", "number");
 
 	//localStorage.setItem = ("stringTwo", '<img id="product-4" src="images/sticker.png" class="img img-responsive">');
 	//document.getElementById("merch-1-cart").innerHTML = localStorage.getItem("stringTwo");
